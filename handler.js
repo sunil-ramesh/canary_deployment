bb3608632fa1eab0e0a7d28c895dc9c6f98f299a
Sunil <skramesh@qwinix.io>
2020-02-07 17:22:32 +0530

    serverless

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
-    body: 'Wait, it is Serverless v1.26.11!'
+    body: 'Wait, it is Serverless v1.26.11! hello'
   };
 
   callback(null, response);
